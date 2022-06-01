7150be167401b93f4dd1f246737f22116864634e
Dmitry Kaveshnikov <kawwa@me.com>
2022-06-01 13:04:01 +0300

    hello

---
+++ TodoList/TodoList/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  TodoList
+//
+//  Created by Dmitry Kaveshnikov on 1/6/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
